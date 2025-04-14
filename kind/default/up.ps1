a0b02dc45dca4a60c6d0dae3fcda93e36b883699
Saulo Costa <saulotarsobc@gmail.com>
2025-04-14 20:26:28 -0300

    update scritp

--- kind/default/up.ps1
+++ kind/default/up.ps1
@@ -1,10 +1,16 @@
+Write-Host "Start kind";
 kind create cluster --name 'meu-cluster' --config 'kind-config.yaml';
+
+Write-host "Get nodes and pods";
 kubectl get nodes -o wide;
 kubectl get pods -o wide;
 
+Write-host "Install ingress-nginx";
 kubectl apply -f "ingress-nginx.yaml";
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s;
 
+Write-host "Configure ingress";
 kubectl apply -f "ingress-config.yaml";
 
+Write-host "Install apps";
 kubectl apply -f "apps-sample.yaml";
